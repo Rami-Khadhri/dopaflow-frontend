8a1dd48066583de72827d9a957e8286a67ac9d54
Rami-khadhri <ramikhd5@gmail.com>
2025-05-20 15:42:00 +0100

    Finished

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -417,7 +417,7 @@ const Reports = () => {
                     <svg className="w-5 h-5 mr-2 group-hover:scale-110 transition-transform" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                     </svg>
-                    Set Global Target
+                    Set Target
                   </button>
                 )}
               </div>
